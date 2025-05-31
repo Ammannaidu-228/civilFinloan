7a577c350ae76afe41885a49c3f119e9026e1aeb
Aman228-dev <ammannaidu.gollapalli.228@gmail.com>
2025-05-31 13:25:50 +0530

    mongo error resolved

--- ui/src/api/backendApi.js
+++ ui/src/api/backendApi.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 
-const BASE_URL = 'http://localhost:3000';
+// const BASE_URL = 'http://localhost:3000';
 
 const  api = axios.create({
-    baseURL: BASE_URL,
+    // baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
